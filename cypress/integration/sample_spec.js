c3d83702d0b2e0c25ebdba1990297dbaff463b46
Lucien Bill <lucien.bill@gmail.com>
2022-01-23 12:48:32 +0100

    add cypress baseUrl

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,4 +2,8 @@ describe('Mon premier test', () => {
     it ('ne fait rien, en fait.', () => {
         expect(true).to.equal(true)
     })
-})
+
+    it ("visite la homepage", () => {
+        cy.visit('')
+    })
+}) 
